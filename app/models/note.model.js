b8ba064f7d90dcc582f1a1c20e025bd0c737fa30
Bjarne Seiffert <svenbjarne@me.com>
2019-09-24 07:38:58 +0200

    changing note model structure

--- app/models/note.model.js
+++ app/models/note.model.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const NoteSchema = mongoose.Schema({
     title: String,
-    content: String
+    content: String,
+    user: String,
+    uuid: String
 }, {
     timestamps: true
 });
